2854607a039e547f41dabd76ef5dad0d11f38c66
Johannes Nägele <johannes-naegele@web.de>
2024-02-21 17:12:17 +0100

    Funktionierende Karte

--- openenergy.py
+++ openenergy.py
@@ -20,19 +20,32 @@ print(df)
 # Assuming 'df' is your DataFrame with the 'geom' column in WKB format
 df['geometry'] = df['geom'].apply(lambda x: wkb.loads(bytes.fromhex(x)))
 
-# Convert DataFrame to GeoDataFrame
+# # Convert DataFrame to GeoDataFrame
 gdf = gpd.GeoDataFrame(df, geometry='geometry')
 
-gdf.plot(cmap='viridis', alpha=0.5)
-plt.show()  # Displays the plot
+# gdf.set_crs(epsg=3857, inplace=True)
+# gdf = gdf.to_crs(epsg=3857)
+
+# ax = gdf.plot(cmap='viridis', alpha=0.5)
+# # ctx.add_basemap(ax, crs=gdf.crs.to_string())
+# ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik)
+# plt.show()  # Displays the plot
 
 gdf.set_crs(epsg=4326, inplace=True)
 gdf = gdf.to_crs(epsg=3857)
+
+# Create a subplot
 fig, ax = plt.subplots(figsize=(10, 10))
+
+# Plot the geometries
+gdf.plot(ax=ax, cmap='viridis', alpha=0.4)
+
+# Set the extent of the plot to the bounds of your geometries
+minx, miny, maxx, maxy = gdf.total_bounds
+ax.set_xlim(minx, maxx)
+ax.set_ylim(miny, maxy)
+
+# Add OSM basemap
 ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik)
-plt.show()
 
-
-# for row in result.json():
-#     # Process the row
-#     print(type(result))
+plt.show()  # Displays the plot

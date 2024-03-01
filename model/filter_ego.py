50772d90b8568afc062f27e498d8897d60ed1a72
Johannes Nägele <johannes-naegele@web.de>
2024-03-01 01:00:23 +0100

    First filter examples

--- model/filter_ego.py
+++ model/filter_ego.py
@@ -2,15 +2,24 @@ import pandas as pd
 
 mypath = "/Users/johannes/Nextcloud/Documents/Uni/FSS_2024/Seminar_Wasserstoff/"
 
+df_bus = pd.read_csv(
+    mypath + "grid__ego_pf_hv_bus/grid__ego_pf_hv_bus.csv"
+)
 df_line = pd.read_csv(
     mypath + "grid__ego_pf_hv_line/grid__ego_pf_hv_line.csv"
+
 )
+print(df_bus)
 print(df_line)
 
 # filter line by >= 200kV
-# df_line_filtered = df_line.loc[df_line['Percentage'] > 200]
-df_line_filtered = df_line.loc[df_line['Percentage'] > 200]
+df_bus_200kV = df_bus.loc[(df_bus['v_nom'] > 200) & (df_bus["scn_name"] == "Status Quo") & (df_bus["version"] == "v0.4.6")]
+bus_ids_220kV = set(df_bus_200kV['bus_id'])
+print(df_bus_200kV)
+df_line_filtered = df_line.loc[(df_line["scn_name"] == "Status Quo") & (df_line["version"] == "v0.4.6")]
+df_line_filtered = df_line_filtered[df_line_filtered['bus0'].isin(bus_ids_220kV) | df_line_filtered['bus1'].isin(bus_ids_220kV)]
 print(df_line_filtered)
+# print(df_line_filtered.loc[[x in df_bus_200kV['bus_id'] for x in df_line_filtered['bus1']]])
 
 
 # filter bus by connection to line
--- model/plot_ego.py
+++ model/plot_ego.py
@@ -20,7 +20,7 @@ def plot_df(df):
     fig, ax = plt.subplots(figsize=(10, 10))
 
     # Plot the geometries
-    gdf.plot(ax=ax, cmap='viridis', alpha=0.4)
+    gdf.plot(ax=ax, color='red', alpha=0.4)
 
     # Set the extent of the plot to the bounds of your geometries
     minx, miny, maxx, maxy = gdf.total_bounds
@@ -38,6 +38,17 @@ df_line = pd.read_parquet(mypath + "line.parquet")
 # plt.show()
 # plot_df(df_transformer)
 # plt.show()
-plot_df(df_line)
+print(df_line["scn_name"].unique())
+df_line = df_line.loc[df_line["scn_name"] == "Status Quo"]
+# plot_df(df_line)
+# plt.show()
+
+# filtered by kV>200
+df_bus_200kV = df_bus.loc[(df_bus['v_nom'] > 200) & (df_bus["scn_name"] == "Status Quo") & (df_bus["version"] == "v0.4.6")]
+bus_ids_220kV = set(df_bus_200kV['bus_id'])
+print(df_bus_200kV)
+df_line_filtered = df_line.loc[(df_line["scn_name"] == "Status Quo") & (df_line["version"] == "v0.4.6")]
+df_line_filtered = df_line_filtered[df_line_filtered['bus0'].isin(bus_ids_220kV) | df_line_filtered['bus1'].isin(bus_ids_220kV)]
+plot_df(df_line_filtered)
 plt.show()
 # plt.savefig("./presentation/termin1/line.png")

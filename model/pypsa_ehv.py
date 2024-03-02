23d32ce4878ab5284fb688920b587c0be1c9fcc2
Johannes Nägele <johannes-naegele@web.de>
2024-03-03 00:14:20 +0100

    PF works

--- model/pypsa_ehv.py
+++ model/pypsa_ehv.py
@@ -1,6 +1,8 @@
 import pypsa
 import filter_ego as fe
 
+# evtl ist vieles mit network.import_series_from_dataframe leichter
+
 # df_line = fe.df_line_200kV.reset_index(drop=True)
 # df_bus = fe.df_bus_200kV.reset_index(drop=True)
 # df_gen = fe.df_gen_200kV.reset_index(drop=True)
@@ -10,6 +12,10 @@ df_line = fe.df_line.reset_index(drop=True)
 df_bus = fe.df_bus.reset_index(drop=True)
 df_gen = fe.df_gen.reset_index(drop=True)
 df_gen_pq = fe.df_gen_pq.reset_index(drop=True)
+df_load = fe.df_load.reset_index(drop=True)
+df_load_pq = fe.df_load_pq.reset_index(drop=True)
+df_trans = fe.df_trans.reset_index(drop=True)
+df_store = fe.df_store.reset_index(drop=True)
 
 network = pypsa.Network()
 
@@ -34,17 +40,56 @@ for i, row in df_gen.iterrows():
         "Generator",
         "My gen {}".format(i),
         bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
-        p_set=100,
-        control="PQ"
+        p_set=df_gen_pq[df_gen_pq["generator_id"] == row["generator_id"]]["p_set"].iloc[0][0], # for first try, take first element
+        control=row["control"]
+        # dispatch
+        # p_nom
+        # p_nom_min
+        # p_nom_max
+        # ...
     )
 
-network.generators
-network.generators.p_set
+print(network.generators)
+print(network.generators.p_set)
 
-# network.add("Load", "My load", bus="My bus 1", p_set=100)
+for i, row in df_load.iterrows():
+    network.add(
+        "Load",
+        "My load {}".format(i),
+        bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
+        p_set=df_load_pq[df_load_pq["load_id"] == row["load_id"]]["p_set"].iloc[0][0], # for first try, take first element
+        sign=row["sign"]
+    )
 
-# network.loads
+print(network.loads)
 # network.loads.p_set
-# network.loads.q_set = 100.0
-# network.pf()
-# network.lines_t.p0
+# network.loads.q_set
+
+for i, row in df_trans.iterrows():
+    network.add(
+        "Transformer",
+        "My transformer {}".format(i),
+        bus0="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus0"]].index[0]),
+        bus1="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus1"]].index[0]),
+        x=row["x"],
+        r=row["r"],
+        g=row["g"],
+        b=row["b"],
+        s_nom=row["s_nom"],
+        # ...
+    )
+
+for i, row in df_store.iterrows():
+    network.add(
+        "StorageUnit",
+        "My storage {}".format(i),
+        bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
+        # dispatch=row["dispatch"],
+        control=row["control"],
+        p_nom=row["p_nom"],
+        # ...
+    )
+
+# Simulate/solve powerflow
+network.pf()
+print(network.lines_t.p0)

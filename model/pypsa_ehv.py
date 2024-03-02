4bf0cd937408c47db9dec228080505723fbe907e
Johannes Nägele <johannes-naegele@web.de>
2024-03-02 01:54:06 +0100

    Extended by generators

--- model/pypsa_ehv.py
+++ model/pypsa_ehv.py
@@ -1,8 +1,15 @@
 import pypsa
 import filter_ego as fe
 
-df_line = fe.df_line_filtered.reset_index(drop=True)
-df_bus = fe.df_bus_200kV.reset_index(drop=True)
+# df_line = fe.df_line_200kV.reset_index(drop=True)
+# df_bus = fe.df_bus_200kV.reset_index(drop=True)
+# df_gen = fe.df_gen_200kV.reset_index(drop=True)
+# df_gen_pq = fe.df_gen_pq_200kV.reset_index(drop=True)
+
+df_line = fe.df_line.reset_index(drop=True)
+df_bus = fe.df_bus.reset_index(drop=True)
+df_gen = fe.df_gen.reset_index(drop=True)
+df_gen_pq = fe.df_gen_pq.reset_index(drop=True)
 
 network = pypsa.Network()
 
@@ -22,10 +29,17 @@ for i, row in df_line.iterrows():
 print(network.buses)
 print(network.lines)
 
-# network.add("Generator", "My gen", bus="My bus 0", p_set=100, control="PQ")
+for i, row in df_gen.iterrows():
+    network.add(
+        "Generator",
+        "My gen {}".format(i),
+        bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
+        p_set=100,
+        control="PQ"
+    )
 
-# network.generators
-# network.generators.p_set
+network.generators
+network.generators.p_set
 
 # network.add("Load", "My load", bus="My bus 1", p_set=100)
 

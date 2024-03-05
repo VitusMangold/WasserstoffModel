a5a6367f7f5287c01b8f513eee10fdb4097d9c72
Johannes Nägele <johannes-naegele@web.de>
2024-03-05 15:31:56 +0100

    Imports of necessary data works

--- europe/entsoe_import.py
+++ europe/entsoe_import.py
@@ -3,7 +3,6 @@ from entsoe import EntsoePandasClient
 
 keyFile = open('europe/.key', 'r')
 consumer_key = keyFile.readline().rstrip()
-print(consumer_key)
 
 client = EntsoePandasClient(api_key=consumer_key)
 
@@ -11,11 +10,16 @@ start = pd.Timestamp('20230101', tz='Europe/Brussels')
 end = pd.Timestamp('20240101', tz='Europe/Brussels')
 country_code = 'DE'  # Germany
 country_code_from = 'DE'  # Germany
-country_code_to = 'FR' # Germany-Luxembourg
-# type_marketagreement_type = 'A01'
-# contract_marketagreement_type = "A01"
-# process_type = 'A51'
+country_code_to = 'FR' # France
 
-# methods that return Pandas Series
-client.query_day_ahead_prices(country_code, start=start, end=end)
-client.query_net_position(country_code, start=start, end=end, dayahead=True)
+country_code = 'DE'
+df_phys_cross = client.query_crossborder_flows(country_code_from, country_code_to, start=start, end=end, export=True, per_hour=True)
+print(df_phys_cross)
+df_phys_cross = client.query_crossborder_flows(country_code_to, country_code_from, start=start, end=end, export=True, per_hour=True)
+print(df_phys_cross)
+
+df_load = client.query_load(country_code, start=start, end=end)
+print(df_load)
+
+df_gen = client.query_generation(country_code, start=start, end=end)
+print(df_gen)

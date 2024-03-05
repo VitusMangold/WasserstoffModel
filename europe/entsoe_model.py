3527b0a09cae4befc32083d7871f92b82ac7ac16
Johannes Nägele <johannes-naegele@web.de>
2024-03-05 16:28:51 +0100

    More entsoe stuff

---
+++ europe/entsoe_model.py
@@ -0,0 +1,6 @@
+# here we should specify the concrete model
+
+to_hydrogen_factor = 0.75 # power to hydrogen, \eta_{2,1}
+storage_costs = ... # in Euro/kWh / h
+per_km_costs = ... # in Euro/km
+power_costs = ... # in Euro/kwh

fa55b4f25e09c75c412d26e21d416ef45476606f
Johannes Nägele <johannes-naegele@web.de>
2024-03-05 01:17:15 +0100

    Strange behavior but should work now

--- model/pypsa_ehv.py
+++ model/pypsa_ehv.py
@@ -1,12 +1,12 @@
+# pylint: disable=no-member
 import pypsa
 import filter_ego as fe
+import pandas as pd
+from warnings import simplefilter
+
+simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 # evtl ist vieles mit network.import_series_from_dataframe leichter
-
-# df_line = fe.df_line_200kV.reset_index(drop=True)
-# df_bus = fe.df_bus_200kV.reset_index(drop=True)
-# df_gen = fe.df_gen_200kV.reset_index(drop=True)
-# df_gen_pq = fe.df_gen_pq_200kV.reset_index(drop=True)
 
 df_line = fe.df_line.reset_index(drop=True)
 df_bus = fe.df_bus.reset_index(drop=True)
@@ -17,10 +17,39 @@ df_load_pq = fe.df_load_pq.reset_index(drop=True)
 df_trans = fe.df_trans.reset_index(drop=True)
 df_store = fe.df_store.reset_index(drop=True)
 
-network = pypsa.Network()
+# Create the date range
+def get_date_range(x):
+    if x == None:
+        return pd.date_range('2023-01-01', periods=1, freq="YE")
+    freq = (365 * 24 * 60) / len(x)
+    return pd.date_range('2023-01-01', periods=len(x), freq="{}min".format(freq))
+
+# truncate for debugging
+# df_gen_pq['p_set'] = df_gen_pq['p_set'].apply(lambda x: pd.Series(x, index=get_date_range(x)))
+# df_load_pq['p_set'] = df_load_pq['p_set'].apply(lambda x: pd.Series(x, index=get_date_range(x)))
+# df_load_pq['q_set'] = df_load_pq['p_set'].apply(lambda x: pd.Series(x, index=get_date_range(x)))
+
+# sanity check
+# print("gen head")
+# print(df_gen_pq.head())
+# print("load head")
+# print(df_load_pq.head())
+
+# set reference coordinate system
+network = pypsa.Network(crs=4326)
+
+# set time points for solution
+snapshots = pd.date_range('2023-01-01', periods=1, freq='H')
+network.set_snapshots(snapshots)
 
 for i, row in df_bus.iterrows():
-    network.add("Bus", "My bus {}".format(i), v_nom=row["v_nom"])
+    network.add(
+        "Bus",
+        "My bus {}".format(i),
+        v_nom=row["v_nom"],
+        x=row["geom"].x,
+        y=row["geom"].y
+    )
 
 for i, row in df_line.iterrows():
     network.add(
@@ -32,38 +61,44 @@ for i, row in df_line.iterrows():
         r=row["r"],
     )
 
-print(network.buses)
-print(network.lines)
+# print(network.buses)
+# print(network.lines)
 
 for i, row in df_gen.iterrows():
+    x = df_gen_pq[df_gen_pq["generator_id"] == row["generator_id"]]["p_set"].iloc[0]
+    if i < 10:
+        print(pd.Series(x, index=get_date_range(x)))
     network.add(
         "Generator",
         "My gen {}".format(i),
         bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
-        p_set=df_gen_pq[df_gen_pq["generator_id"] == row["generator_id"]]["p_set"].iloc[0][0], # for first try, take first element
-        control=row["control"]
-        # dispatch
-        # p_nom
-        # p_nom_min
-        # p_nom_max
+        p_set=pd.Series(x, index=get_date_range(x)),
+        control=row["control"],
+        p_nom=row["p_nom"],
+        p_nom_min=row["p_nom_min"],
+        p_nom_max=row["p_nom_max"]
         # ...
+        # dispatch
     )
 
-print(network.generators)
+# print(network.generators)
 print(network.generators.p_set)
 
 for i, row in df_load.iterrows():
+    x_p = df_load_pq[df_load_pq["load_id"] == row["load_id"]]["p_set"].iloc[0]
+    x_q = df_load_pq[df_load_pq["load_id"] == row["load_id"]]["q_set"].iloc[0]
     network.add(
         "Load",
         "My load {}".format(i),
         bus="My bus {}".format(df_bus[df_bus["bus_id"] == row["bus"]].index[0]),
-        p_set=df_load_pq[df_load_pq["load_id"] == row["load_id"]]["p_set"].iloc[0][0], # for first try, take first element
+        p_set=pd.Series(x_p, index=get_date_range(x_p)),
+        q_set=pd.Series(x_q, index=get_date_range(x_q)),
         sign=row["sign"]
     )
 
-print(network.loads)
-# network.loads.p_set
-# network.loads.q_set
+# print(network.loads)
+print(network.loads.p_set)
+print(network.loads.q_set)
 
 for i, row in df_trans.iterrows():
     network.add(
@@ -92,4 +127,6 @@ for i, row in df_store.iterrows():
 
 # Simulate/solve powerflow
 network.pf()
-print(network.lines_t.p0)
+# print(network.lines_t.p0)
+
+# pypsa.Network.export_to_netcdf()

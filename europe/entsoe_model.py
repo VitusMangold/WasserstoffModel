0f37c174d4dbbebae334a5219be71926f7c36484
Johannes Nägele <johannes-naegele@web.de>
2024-04-17 14:21:42 +0200

    Refactor overproduction: Use new cost factor instead of the same as punishment

--- europe/entsoe_model.py
+++ europe/entsoe_model.py
@@ -20,19 +20,24 @@ time_horizon = 20 # in years
 line_length = 2000.0 # in km
 power_building_costs = 42e9 / (4e6) # in €/kW, Nord-Sued-Link
 h2_building_costs = (7.5e9 / (165e9 / (365 * 24))) * (line_length / 1230) # in €/kW
+h2_building_costs = h2_building_costs # to avoid h2, take h2_building_costs*10000
+power_building_costs = power_building_costs # to get more building, take power_building_costs / 4
 # extrapolate GW Nordstream 2 to Spain times costs
 # 165000 GWh und wir betrachten ein Jahr; 7.5 Mrd Euro Baukosten
 to_h2_factor = 0.25 * 0.6 # power to hydrogen
 transport_loss_power = 0.05 # per 1000 km
 transport_loss_h2 = 0.0 # per 1000 km
 # power_price_conventional = 0.3000 # in Euro/kWh
-power_price_conventional = 0.8 # in Euro/kWh
+power_price_conventional = 0.3 # in Euro/kWh
 power_price_renewable = 0.08 # in Euro/kWh
+power_price_overproduction = 0.05 # in Euro/kWh
 
-def conventional_costs(missing_power):
+def conventional_costs(missing_power, too_much_power):
     """ Models the costs arising from having to fill net energy gap by conventional power generation. """
     # FIXME:
-    return np.sum(missing_power[~np.isnan(missing_power)]) * power_price_conventional
+    missing_costs = np.sum(missing_power[~np.isnan(missing_power)]) * power_price_conventional
+    overproduction_reward = np.sum(too_much_power[~np.isnan(too_much_power)]) * power_price_conventional
+    return missing_costs - overproduction_reward
 
 def renewable_costs(generation):
     """ Power costs of renewables. """
@@ -96,16 +101,13 @@ def costs(capacity_power, capacity_h2, share_renewable_de, share_renewable_other
     )
     absolute_inflow = pointwise_inflow(de_net, other_net)
 
-    missing_power_de = -(de_net.mask(de_net > 0.0, 0.0).to_numpy() + np.where(de_net < 0.0, absolute_inflow, 0.0))
     # where cond is true, keep value
-    missing_power_other = -(other_net.mask(other_net > 0.0, 0.0).to_numpy() + np.where(other_net < 0.0, absolute_inflow, 0.0))
-    # print(pd.DataFrame(missing_power_de + missing_power_other).describe())
-    # print(np.count_nonzero(np.isnan(missing_power_de + missing_power_other)))
-    c_costs = conventional_costs(missing_power_de + missing_power_other)
+    missing_power_de = np.where(de_net < 0.0, absolute_inflow, 0.0)
+    missing_power_other = np.where(other_net < 0.0, absolute_inflow, 0.0)
+    too_much_power_de = de_net.mask(de_net > 0.0, 0.0).to_numpy()
+    too_much_power_other = other_net.mask(other_net > 0.0, 0.0).to_numpy()
+    c_costs = conventional_costs(missing_power_de + missing_power_other, too_much_power_de + too_much_power_other)
     r_costs = renewable_costs(hypothetical_de + hypothetical_other)
     power_l_costs = power_line_costs(capacity_power)
     h2_l_costs = h2_line_costs(capacity_h2)
-    # r_costs = 0.0
-    # power_l_costs = 0.0
-    # h2_l_costs = 0.0
     return c_costs + power_l_costs + h2_l_costs + r_costs

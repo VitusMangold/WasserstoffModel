8f61d156517704e11bf20a3b08acd0864dfbc308
Johannes Nägele <johannes-naegele@web.de>
2024-04-22 14:43:18 +0200

    Add graph to new model

--- europe/entsoe_multiple_model.py
+++ europe/entsoe_multiple_model.py
@@ -1,4 +1,5 @@
 import entsoe_multiple_preprocess as emp
+import networkx as nx
 
 for key in emp.loads_from_json:
     print(key)
@@ -7,3 +8,10 @@ for key in emp.loads_from_json:
 for key in emp.gens_from_json:
     print(key)
     print(emp.gens_from_json[key])
+
+G = nx.DiGraph()
+
+core_country = "DE"
+for key in emp.loads_from_json:
+    if key != core_country:
+        G.add_edge(core_country, key, capacity=0)

c9f78305459af59599ab81cf6e6b679eb2969055
Johannes Nägele <johannes-naegele@web.de>
2024-04-23 04:57:07 +0200

    use constants

---
+++ europe/constants.py
@@ -0,0 +1,6 @@
+time_horizon = 20 # in years
+line_length = 2000.0 # in km
+power_building_costs = 42e9 / (4e6) # in €/kW, Nord-Sued-Link
+power_price_conventional = 0.3 # in Euro/kWh
+power_price_renewable = 0.08 # in Euro/kWh
+power_price_overproduction = 0.04 # in Euro/kWh

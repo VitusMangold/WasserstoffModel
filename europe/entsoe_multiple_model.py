0620f1a8cad52be48fd3cca020ea8a91277961e2
Johannes Nägele <johannes-naegele@web.de>
2024-04-30 11:06:05 +0200

    Daten

--- europe/entsoe_multiple_model.py
+++ europe/entsoe_multiple_model.py
@@ -4,8 +4,19 @@ from networkx.algorithms.flow import shortest_augmenting_path
 import auto_start_end
 import constants
 import pandas as pd
+import matplotlib.pyplot as plt
 
 core_country = "DE"
+
+def init_graph(capacities):
+    graph = nx.DiGraph()
+    net_dict = {key: pd.Series(0.0, index=loads[key].index) for key in loads}
+
+    # Set line capacities
+    for key, value in capacities.items(): # should not be DE
+        graph.add_edge(core_country, key, capacity=value)
+        graph.add_edge(key, core_country, capacity=value)
+    return graph, net_dict  
 
 def calculate_net_flow(flow_dict, production_per_hour, consumption_per_hour,  net_dict, snapshot):
     """ Calculate the net flow for each country given the flow."""
@@ -47,13 +58,7 @@ def costs(capacities, share_renewables):
     hypothetical = {key: renewables[key] * (gen_total[key] / renewables[key].sum()) * share_renewables[key] for key in renewables}
 
     # Initialize graph
-    G = nx.DiGraph()
-    net_dict = {key: pd.Series(0.0, index=loads[key].index) for key in loads}
-
-    # Set line capacities
-    for key, value in capacities.items(): # should not be DE
-        G.add_edge(core_country, key, capacity=value)
-        G.add_edge(key, core_country, capacity=value)
+    graph, net_dict = init_graph(capacities)
     # FIXME: Richtung zu DE
 
     for snapshot, _ in enumerate(hypothetical["DE"]):
@@ -61,9 +66,9 @@ def costs(capacities, share_renewables):
         consumption_per_hour = {key: loads[key].iloc[snapshot] for key in loads}
 
         # Set power generation and consumption
-        G = auto_start_end.start_end_node(G, production_per_hour, consumption_per_hour)
+        graph = auto_start_end.start_end_node(graph, production_per_hour, consumption_per_hour)
 
-        _, flow_dict = nx.maximum_flow(G, "Start", "Ziel", flow_func=shortest_augmenting_path)
+        _, flow_dict = nx.maximum_flow(graph, "Start", "Ziel", flow_func=shortest_augmenting_path)
         calculate_net_flow(flow_dict, production_per_hour, consumption_per_hour, net_dict, snapshot)
 
     gen_renewable_costs = sum(
@@ -74,11 +79,37 @@ def costs(capacities, share_renewables):
     
     return gen_renewable_costs + net_power_costs + building_costs
 
-hypothetical = {key: renewables[key] * (gen_total[key] / renewables[key].sum()) * 1.0 for key in renewables}
+# Plot capacities
+results = ({'BE': 2.27628932864678, 'CH': 5613.480849211652, 'CZ': 10135.834209017168, 'DK': 1152.3246060422075, 'FR': 23997.617462034075, 'LU': 5431.8990356233335, 'NL': 43764.21927149885, 'PL': 19735.593078375066}, {'BE': 1.007471114494228, 'CH': 1.0575945698642508, 'CZ': 1.5774695858438257, 'DE': 0.0014234443207886919, 'DK': 0.6428696374671219, 'FR': 0.9610810203137954, 'LU': 2.7549141162176634, 'NL': 1.1600210850270312, 'PL': 1.98599188940605})
+graph, _ = init_graph(
+    {key: int(round(value, 0)) for key, value in results[0].items()}
+)
+pos = {"Start": (0.5, 2), "DE": (0.5, 0), "FR": (0.2, -0.5), "PL": (1, 1), "AU": (0.75, -1), "DK": (0.4, 1),"CZ": (1,0), "CH": (0.25,-1), "NL": (0.2,0.8), "BE": (0.1,0.5), "LU": (0.1,0.25),
+       "Ziel": (0.5, -2)}
+
+# nodes
+nx.draw_networkx_nodes(graph, pos, node_size=500)
+
+# edges
+for (u, v, d) in graph.edges(data=True):
+    nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=d["capacity"]/5000)
+
+# node labels
+nx.draw_networkx_labels(graph, pos, font_size=20, font_family="sans-serif")
+# edge capacity labels
+edge_labels = nx.get_edge_attributes(graph, "capacity")
+nx.draw_networkx_edge_labels(graph, pos, edge_labels)
+
+ax = plt.gca()
+ax.margins(0.08)
+plt.axis("off")
+plt.tight_layout()
+plt.savefig("./presentation/termin3/capacities.pdf")
+plt.show()
 
 # Frankreich: Atom gleich lassen
 # Eine Kante Fluss, Saldo positiv, Saldo negativ
-# Snapshot pro Stunde: Bild des Graphe
+# Snapshot pro Stunde: Bild des Graphen
 # Tabelle: Spalte Total Erzeugung / Verbrauch
 # Grafik: Graphen für alle Länder mit Kapazitäten als Kantendicken
 # Kosten Ausgangszustand vs. optimiert; das auch aufgeschlüsselt

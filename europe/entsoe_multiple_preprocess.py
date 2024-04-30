f28a4dbfaaa3043cb96df0c7236f605793126c19
Johannes Nägele <johannes-naegele@web.de>
2024-04-30 11:06:05 +0200

    Daten

--- europe/entsoe_multiple_preprocess.py
+++ europe/entsoe_multiple_preprocess.py
@@ -124,6 +124,7 @@ plt.title("Hypothetical generation minus load")
 end_time = '2023-01-07'
 plt.show()
 scale = {'BE': 1.007471114494228, 'CH': 1.0575945698642508, 'CZ': 1.5774695858438257, 'DE': 0.0014234443207886919, 'DK': 0.6428696374671219, 'FR': 0.9610810203137954, 'LU': 2.7549141162176634, 'NL': 1.1600210850270312, 'PL': 1.98599188940605}
+print({key: round(value, 3) for key, value in scale.items()})
 hypothetical = {key: value * scale[key] for key, value in hypothetical.items()}
 sum_hypo_new = sum(value.resample('1h').mean() for value in hypothetical.values())
 sum_loads = sum(value.resample('1h').mean() for value in loads.values())
@@ -135,3 +136,7 @@ plt.xticks(rotation=-20)
 plt.title("Hypothetical generation vs. load")
 plt.savefig("./presentation/termin3/optimized_sum_1_week.pdf")
 plt.show()
+
+print(
+    {key: round(value.resample('1h').mean().loc['2023-01-01':end_time].sum() / loads[key].resample('1h').mean().loc['2023-01-01':end_time].sum(), 4) for key, value in hypothetical.items()}
+)

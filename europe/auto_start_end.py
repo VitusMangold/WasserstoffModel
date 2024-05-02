0be06a0c46fd667c68cc7c0da69422343c9341ba
Johannes Nägele <johannes-naegele@web.de>
2024-05-02 12:52:12 +0200

    Changes from Lena

--- europe/auto_start_end.py
+++ europe/auto_start_end.py
@@ -1,7 +1,37 @@
-def start_end_node(graph, node_production, node_consumption):
+def start_end_node(graph):
     graph_new = graph.copy()
     for node in graph.nodes():
         if node != "Start" and node != "Ziel":
-            graph_new.add_edge("Start", node, capacity=node_production[node])  # Verbinde den Startknoten mit jedem anderen Knoten
-            graph_new.add_edge(node, "Ziel", capacity=node_consumption[node])
-    return graph_new
+            # Verbinde den Startknoten mit jedem anderen Knoten
+            graph_new.add_edge("Start", node, capacity= 0)
+            graph_new.add_edge(node, "Ziel", capacity= 0)
+    return graph_new, list(graph_new.nodes())
+
+
+def start_end_node_capacity(graph, node_production, node_consumption, node_list):
+    
+    for node in node_list:
+        if node != "Start" and node != "Ziel":
+            # add the new capacity to the edge
+            graph["Start"][node]["capacity"] = node_production[node]  
+            graph[node]["Ziel"]["capacity"] = node_consumption[node]
+    return graph
+
+
+# Erstellen eines 2D-Arrays mit Listen in Listen
+neighbors = [
+    ["NL", "BE", "LU", "FR", "CH", "AT", "DK", "PL", "CZ"],
+    ["BE", "DE", "DK"],
+    ["LU", "NL", "DE", "FR"],
+    ["FR", "BE", "DE"],
+    ["ES", "IT", "CH", "DE", "LU", "BE"],
+    ["FR"],
+    ["FR", "AU", "CH"],
+    ["AT", "DE", "IT", "FR"],
+    ["DE", "CH", "IT", "CZ"],
+    ["DE", "AT", "PL"],
+    ["CZ", "DE"],
+    ["DE", "NL"]
+]
+
+order_list_neighbors = ["DE", "NL", "BE", "LU", "FR", "ES", "IT", "CH", "AT", "CZ", "PL", "DK"]

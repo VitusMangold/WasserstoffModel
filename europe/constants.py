c8f4537489af2635d649a0efa3bba289157afa1b
Johannes Nägele <johannes-naegele@web.de>
2024-05-02 12:37:43 +0200

    Adapt constants; time horizon for one week

--- europe/constants.py
+++ europe/constants.py
@@ -1,8 +1,10 @@
-time_horizon = 20 # in years
-power_building_costs = 14.3 # in €/(kW * km), Nord-Sued-Link
-power_price_conventional = 0.3 # in Euro/kWh
-power_price_renewable = 0.08 # in Euro/kWh
-power_price_overproduction = 0.10 # in Euro/kWh
+# The time series are in MW and not in kW, it does not make a difference though
+mw_to_kw = 1000.0
+time_horizon = 20 * 52 # in years (52 weeks per year))
+power_building_costs = 14.3 * mw_to_kw # in €/(mW * km), Nord-Sued-Link
+power_price_conventional = 0.3 * mw_to_kw # in Euro/mWh
+power_price_renewable = 0.08 * mw_to_kw # in Euro/mWh
+power_price_overproduction = 0.10 * mw_to_kw # in Euro/mWh
 
 distances = {
     "BE": 330,

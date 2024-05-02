478e9ae6c727a492efce5910eb42be0ed74175d6
Lena Radmer <lena-rene.radmer@dlr.de>
2024-05-02 13:30:00 +0200

    Adding AT, ES, IT to shares array

--- europe/entsoe_multiple_results.py
+++ europe/entsoe_multiple_results.py
@@ -7,7 +7,7 @@ from scipy.optimize import minimize
 import entsoe_multiple_model as em
 
 capacities = {"BE" : 1000, "CH" : 1000, "CZ" : 1000, "DK" : 1000, "FR" : 1000, "LU" : 1000, "NL" : 1000, "PL" : 1000}
-shares = {"BE" : 1.0, "CH" : 1.0, "CZ" : 1.0, "DE" : 1.0, "DK" : 1.0, "FR" : 1.0, "LU" : 1.0, "NL" : 1.0, "PL" : 1.0}
+shares = {"BE" : 1.0, "CH" : 1.0, "CZ" : 1.0, "DE" : 1.0, "DK" : 1.0, "FR" : 1.0, "LU" : 1.0, "NL" : 1.0, "PL" : 1.0, "AT" : 1.0, "IT" : 1.0, "ES" : 1.0}
 
 ## Minimize in all (2n - 1) dimensions
 

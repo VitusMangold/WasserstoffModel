c683cde8f43fc2b67aa085edae515ca85dc6db55
Johannes Nägele <johannes-naegele@web.de>
2024-05-08 18:31:38 +0200

    Data load in completed

--- europe/fast_model/model.jl
+++ europe/fast_model/model.jl
@@ -1,5 +1,7 @@
 using DataFrames
 using JSON
+using Dates
+using TimeZones
 using Pipe
 using Optim
 
@@ -8,21 +10,26 @@ data_json = JSON.parsefile("energy_data.json")
 gens = data_json["generation"]
 loads = data_json["loads"]
 
+function to_date(str)
+    ## For ISO 8601 datetime string with UTC indicator
+
+    # Parse the string using the correct ISO format with Z for UTC
+    datetime_obj = DateTime(str, dateformat"yyyy-mm-ddTHH:MM:SS.sss\Z")
+
+    # # Parse the string to a DateTime object using the custom format
+    zoned = ZonedDateTime(datetime_obj, tz"UTC")
+    return DateTime(TimeZones.astimezone(zoned, tz"Europe/Berlin"))
+end
+
 to_df(data) = @pipe data |>
     (
         k = keys(_);
         DataFrame(
-            "Date" => collect(keys(first(values(_)))),
+            "Date" => to_date.(collect(keys(first(values(_))))),
             [indicator => collect(values(_[indicator])) for indicator in k]...
         )
     ) |>
-    transform(_, names(_)[2] => ByRow(passmissing(x -> isnothing(x) ? missing : convert(Float64, x))), renamecols=false)
-
-@pipe gens["ES"] |> JSON.parse(_)
-test = ...
-@pipe gens["FR"] |>
-    JSON.parse(_)
-    
+    transform(_, names(_)[2] => ByRow(passmissing(x -> isnothing(x) ? missing : convert(Float64, x))), renamecols=false)    
 
 function convert_to_dfs!(data)
     for key in keys(data)
@@ -36,4 +43,3 @@ end
 
 convert_to_dfs!(loads)
 convert_to_dfs!(gens)
-loads

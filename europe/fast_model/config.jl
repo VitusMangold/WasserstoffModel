89f1e7a75b4afcfd75ebbd10c7c5a7e33944c207
Johannes Nägele <johannes-naegele@web.de>
2024-05-16 14:31:35 +0200

    Refactor into optimum

--- europe/fast_model/config.jl
+++ europe/fast_model/config.jl
@@ -23,6 +23,6 @@ model = MaxflowModel(
     time_horizon = 20, # in years
     power_building_costs = 5.0 * mw_to_kw, # in €/(mW * km), Nord-Sued-Link
     power_price_conventional = 0.20 * mw_to_kw, # in Euro/mWh
-    power_price_renewable = 0.08 * mw_to_kw, # in Euro/mWh
+    power_price_renewable = 0.07 * mw_to_kw, # in Euro/mWh
     power_price_overproduction = 0.10 * mw_to_kw, # in Euro/mWh
 )

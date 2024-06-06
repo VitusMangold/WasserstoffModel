75dd6038b233cdcc5adcb4e31c1f46d686fd3fbd
Johannes Nägele <johannes-naegele@web.de>
2024-06-07 01:53:25 +0200

    Add docstrings

--- europe/fast_model/solver.jl
+++ europe/fast_model/solver.jl
@@ -1,3 +1,4 @@
+""" Initialize and return the distance matrix for the MOA problem."""
 function init_mats(model)
     n_ids = length(keys(model.config.ids))
     distance_matrix = ones(n_ids, n_ids)
@@ -15,16 +16,11 @@ function init_mats(model)
     return distance_matrix
 end
 
-function init_all_solvers!(model)
-    dist_mat = init_mats(model)
-    solvers = [create_solver(model, dist_mat, 1, 2) for _ in axes(model.loads, 1)]
-    n = size(dist_mat, 1)
-    Containers.@container(flow[i = 1:n, j = 1:n; near(model, i, j)], 1.0)
-    flows = fill(flow, length(solvers))
-    append!(model.solvers, solvers)
-    append!(model.flows, flows)
-end
+"""
+Create solver for the MOA problem.
 
+In the current implementation we have a tuple (two solvers) for each snapshot.
+"""
 function create_solver(model, distance_matrix, source, target)
     solver1 = Model(() -> DiffOpt.diff_optimizer(HiGHS.Optimizer))
     solver2 = Model(() -> DiffOpt.diff_optimizer(HiGHS.Optimizer))
@@ -34,14 +30,15 @@ function create_solver(model, distance_matrix, source, target)
 
     function set_common_constraints!(solver)
         # Variable definition
-        @variable(solver, f[i = 1:n, j = 1:n; near(model, i, j)] >= 0)
+        @variable(solver, f[i = 1:n, j = 1:n; near(model, i, j)] ≥ 0)
         # Capacity constraints are modified in the max_flow_lp function; right now we set all to magic_number
-        @constraint(solver, upper[i = 1:n, j = 1:n; near(model, i, j)], f[i, j] <= magic_number)
+        @constraint(solver, upper[i = 1:n, j = 1:n; near(model, i, j)], f[i, j] ≤ magic_number)
         # Flow conservation constraints with losses
-        @constraint(solver, [i = 1:n; i != source && i != target], sum(f[:, i]) == sum(distance_matrix[j, i] * f[i, j] for j in 1:n if near(model, i, j)))
+        @constraint(solver, [i = 1:n; i ≠ source && i ≠ target], sum(f[:, i]) == sum(distance_matrix[j, i] * f[i, j] for j in 1:n if near(model, i, j)))
     end
 
     set_common_constraints!.([solver1, solver2])
+
     # Maximize used energy
     @objective(solver1, Max, sum(solver1[:f][:, 2]))
     
@@ -52,6 +49,22 @@ function create_solver(model, distance_matrix, source, target)
     return (solver1, solver2)
 end
 
+""" Initialize MOA solver for each snapshot. """
+function init_all_solvers!(model)
+    dist_mat = init_mats(model)
+    solvers = [create_solver(model, dist_mat, 1, 2) for _ in axes(model.loads, 1)]
+    n = size(dist_mat, 1)
+    Containers.@container(flow[i = 1:n, j = 1:n; near(model, i, j)], 1.0) # for backpropagation
+    flows = fill(flow, length(solvers))
+    append!(model.solvers, solvers)
+    append!(model.flows, flows)
+end
+
+"""
+Check whether two nodes are connected by a pipe.
+
+This includes the start and end nodes connected via generation/load.
+"""
 function near(model, x, y)
     name_x = findfirst(isequal(x), model.config.ids)
     name_y = findfirst(isequal(y), model.config.ids)
@@ -66,7 +79,7 @@ function near(model, x, y)
         end
     end
 
-    return (name_x == "start" != name_y) ⊻ (name_y == "end" != name_x)
+    return (name_x == "start" ≠ name_y) ⊻ (name_y == "end" ≠ name_x)
 end
 
 function set_bounds!(con1, con2, capacities, hypo, loads, pipes, ids, snapshot)
@@ -102,6 +115,7 @@ function set_bounds!(con1, con2, capacities, hypo, loads, pipes, ids, snapshot)
     return nothing
 end
 
+""" Return the flow matrix for our MOA-LP-model. """
 function max_flow_lp(capacities, model, hypo, snapshot)
     solver = model.solvers[snapshot]
 

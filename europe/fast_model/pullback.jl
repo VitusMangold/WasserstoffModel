77e52de5f88d5e4aea22033cc201ca7049774718
Johannes Nägele <johannes-naegele@web.de>
2024-06-10 20:58:12 +0200

    This does for capacities more or less work

--- europe/fast_model/pullback.jl
+++ europe/fast_model/pullback.jl
@@ -21,79 +21,141 @@ function gradient(::typeof(sum_costs), model::MaxflowModel, capacities, share_re
     return dnet_mat, dcapacities, dshare_ren
 end
 
-""" Reverse diff set_bounds! """
-function gradient(::typeof(set_bounds!),
-    grad1, grad2, capacities, dcapacities, hypo, dhypo, model, snapshot
-)
-    g1 = Enzyme.make_zero(grad1)
-    g2 = Enzyme.make_zero(grad2)
-    Enzyme.autodiff(
-        Reverse,
-        set_bounds!,
-        Const,
-        Duplicated(g1, grad1),
-        Duplicated(g2, grad2),
-        Duplicated(capacities, dcapacities),
-        Duplicated(hypo, dhypo),
-        Const(model.loads),
-        Const(model.config.pipes),
-        Const(model.config.ids),
-        Const(snapshot)
-    )
-end
-
 """ Reverse diff max_flow_lp at a given snapshot """
-function gradient(::typeof(max_flow_lp), dflow, model, snapshot)
+function gradient(::typeof(max_flow_lp), dflow, model, capacities, dcapacities, hypo, dhypo, log_dist, snapshot)
 
-    solver = model.solvers[snapshot]
+    order(x, y) = x < y ? (x, y) : (y, x)
 
-    # Backpropagate the flow matrix impact to the achieved max flow
-    MOI.set.(solver[2], DiffOpt.ReverseVariablePrimal(), solver[2][:f], dflow)
-    DiffOpt.reverse_differentiate!(solver[2])
-    obj_exp = MOI.get.(solver[2], DiffOpt.ReverseConstraintFunction(), solver[2][:result])
-    grad = -JuMP.constant.(obj_exp) # no idea why the minus is needed here
+    # solver = model.solvers[snapshot]
+    flow = model.flows[snapshot]
+    Is, Js, Vs = [k1 for ((k1, k2), v) in flow.data], [k2 for ((k1, k2), v) in flow.data], [v for ((k1, k2), v) in flow.data]
+    F = sparse(Is, Js, Vs) # sparse array from flow
+    function connect!(F, transpose=false)
+        for ((i, j), val) in flow.data
+            F[i, j] = log_dist[i, j] # default value
+            if i == 1 # gen
+                if hypo[snapshot, j] == val
+                    if !transpose
+                        F[i, j] = Inf
+                    else
+                        F[j, i] = Inf
+                    end
+                end
+                # -> schaue schnellster Weg von Start zu Ende und nehme das als Fluss/Ableitung von Start los
+            elseif j == 2 # load
+                if model.loads[snapshot, i] == val
+                    if !transpose
+                        F[i, j] = Inf
+                    else
+                        F[j, i] = Inf
+                    end
+                end
+                # -> schaue schnellster Weg von Land zu Ende und nehme das als Fluss/Ableitung von Start los
+            else # Leitung A -> B
+                cap_i, cap_j = order(i, j)
+                if isapprox(capacities[cap_i, cap_j], val) || model.net_mat[snapshot, cap_j] >= 0 # pipe full or target is already satisfied
+                    F[i, j] = Inf
+                end
+                # -> schaue schnellster Weg von Land B zu Ende und nehme das mal (A -> B) als Fluss/Ableitung
+            end
+        end
+    end
+    connect!(F)
 
-    # Backpropagate the flow matrix impact to the capacities/hypo
-    obj_exp = MOI.get.(solver[2], DiffOpt.ReverseConstraintFunction(), solver[2][:upper])
-    grad2 = -JuMP.constant.(obj_exp) # no idea why the minus is needed here
+    # Graph to findest shortest paths
+    graph1 = DiGraph(14)
+    for (country, neighbors) in distances
+        x = model.config.ids[country]
+        for (neighbor, _) in neighbors
+            y = model.config.ids[neighbor]
+            add_edge!(graph1, x, y)
+            add_edge!(graph1, y, x)
+        end
+        add_edge!(graph1, model.config.ids["start"], x)
+        add_edge!(graph1, x, model.config.ids["end"])
+    end
 
-    # Backpropagate the achieved max flow to the capacities/hypo
-    MOI.set.(solver[1], DiffOpt.ReverseVariablePrimal(), solver[1][:f][:, 2], grad) # they have all the same gradient
-    DiffOpt.reverse_differentiate!(solver[1])
-    obj_exp = MOI.get.(solver[1], DiffOpt.ReverseConstraintFunction(), solver[1][:upper])
-    grad1 = -JuMP.constant.(obj_exp) # no idea why the minus is NOT needed here
-    return grad1, grad2
+    graph2 = DiGraph(14)
+    for (country, neighbors) in distances
+        x = model.config.ids[country]
+        for (neighbor, _) in neighbors
+            y = model.config.ids[neighbor]
+            add_edge!(graph2, x, y)
+            add_edge!(graph2, y, x)
+        end
+        # Umgekehrt
+        add_edge!(graph2, x, model.config.ids["start"])
+        add_edge!(graph2, model.config.ids["end"], x)
+    end
+
+    # Direction 1
+    res1 = dijkstra_shortest_paths(graph1, 1, F)
+
+    # Direction 2
+    res2 = dijkstra_shortest_paths(graph2, 2, F')
+
+    # We look at pipe from i to j and see which flows change
+    function propagate_forward(res1, res2, i, j, dflow)
+        cap_i, cap_j = order(i, j)
+
+        # Trace to start
+        last_parent = j
+        parent = i
+        println("i: ", i, " j: ", j) # 6, 3
+        while parent != 0
+            # if i == 3
+            #     println("parent: ", parent, " last_parent: ", last_parent)
+            # end
+            dcapacities[cap_i, cap_j] += dflow[parent, last_parent] #/ exp(F[parent, last_parent])
+            last_parent = parent
+            parent = res1.parents[parent]
+        end
+        # Trace to end
+        last_child = j
+        child = res2.parents[j]
+        while child != 0
+            # if i == 3
+            #     println("child: ", child, " last_child: ", last_child)
+            # end
+            dcapacities[cap_i, cap_j] += dflow[last_child, child] #/ exp(F[parent, last_parent])
+            last_child = child
+            child = res2.parents[child]
+        end
+    end
+
+    for (country, vals) in model.config.pipes
+        row = model.config.ids[country]
+        for neighbor in vals
+            col = model.config.ids[neighbor]
+            if res2.dists[col] < Inf && res1.dists[row] < Inf # can we load from col and gen from row?
+                propagate_forward(res1, res2, row, col, dflow)
+            elseif res2.dists[row] < Inf && res1.dists[col] < Inf # can we load from row and gen from col?
+                propagate_forward(res2, res1, col, row, dflow') # reverse roles
+            end
+        end
+    end
+
+    # dhypo[snapshot, i] += val
+
+    return graph1, graph2, F, res1, res2
 end
+
+# IT, AT, ES, DK (6, 8, 9, 11)
+post_dhypo = deepcopy(pre_dhypo)
+post_dhypo .= 0
+pre_dcapacities .= 0
+g1, g2, F, r1, r2 = gradient(max_flow_lp, pre_dflows[1], model_base, t_cap, pre_dcapacities, model_base.hypothetical, post_dhypo, log.(init_mats(model_base)), 1)
+r1.dists
+r2.dists
+r2.parents
 
 """ Reverse diff max_flow_lp """
 function gradient(::typeof(max_flow_lp), dflows, model, capacities, dcapacities, hypo, dhypo)
 
-    # Initialize grads
-    res = gradient(max_flow_lp, dflows[1], model, 1)
-    n_chunks = 8
-    buffer = [[deepcopy(res) for _ in 1:n_chunks] for _ in 1:Threads.nthreads()]
-    global pre_buffer = deepcopy(buffer)
-
-    # Fill grads
-    lock = ReentrantLock();
-    @floop for j in 1:div(length(axes(model.hypothetical, 1)), n_chunks)
-        i = Threads.threadid()
-        for chunk in 1:n_chunks
-            snapshot = (j - 1) * n_chunks + chunk
-            grad1, grad2 = gradient(max_flow_lp, dflows[snapshot], model, snapshot)
-            buffer[i][chunk] = grad1, grad2
-        end
-        Threads.lock(lock) do
-            for chunk in 1:n_chunks
-                snapshot = (j - 1) * n_chunks + chunk
-                grad1, grad2 = buffer[i][chunk]
-                gradient(set_bounds!,
-                    grad1, grad2, capacities, dcapacities, hypo, dhypo, model, snapshot
-                )
-            end
-        end
+    log_losses = log.(init_mats(model_base))
+    for snap in axes(dflows, 1)
+        F, r1, r2 = gradient(max_flow_lp, dflows[i], model, capacities, dcapacities, hypo, dhypo, log_losses, i)
     end
-    global post_buffer = deepcopy(buffer)
 end
 
 """ Reverse diff costs """
@@ -105,6 +167,7 @@ function gradient(::typeof(costs), model::MaxflowModel, capacities, share_ren)
     global pre_dshare_ren = deepcopy(dshare_ren) # positiv -> Kosten steigen
     hypo = similar(model.hypothetical)
     scale_up!(hypo, model.hypothetical, share_ren)
+    global pre_hypo = deepcopy(hypo)
     flows = model.flows
     dhypo = Enzyme.make_zero(hypo.array)
     dflows = Enzyme.make_zero(flows)
